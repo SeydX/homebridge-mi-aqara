defe371bf1a4aff97b4c233f91cddcdede385025
DSchumacher2104 <29118191+DSchumacher2104@users.noreply.github.com>
2019-08-01 10:10:14 +0200

    Update GatewayParser.js

--- parser/GatewayParser.js
+++ parser/GatewayParser.js
@@ -1,5 +1,6 @@
 const DeviceParser = require('./DeviceParser');
 const AccessoryParser = require('./AccessoryParser');
+const util = require('util');
 
 class GatewayParser extends DeviceParser {
     constructor(platform) {
